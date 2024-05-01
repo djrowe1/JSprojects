592f5845dee09b73b0903a4ad7ac36b5e878b808
Dave Rowe <djrowe1@yahoo.com>
2024-05-01 16:36:21 -0400

    Modified file structure and added basic code

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 function showMessage() {
-  alert("Hello Dave!");
+  let first = "Dave",
+    last = "Rowe";
+  first = prompt("Enter first name: ");
+  last = prompt("Enter last name: ");
+  alert(first + " " + last + "!!");
+  //console.log("Hello developer world!");
 }
 //showMessage();
 document.addEventListener("DOMContentLoaded", showMessage);
